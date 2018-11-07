28c81d17382f8563fe5b4e8016145dc33efe0673
William Pereira <william.pereira@me.com>
2018-11-06 23:09:21 -0200

    finished oauth

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'There' });
-});
-
+require('./services/passport.js')
+require('./routes/authRoutes.js')(app)
 
 app.listen(port);
