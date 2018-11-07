7d4d591d5d0fa4665074bdb17f2275fb883358a2
William Pereira <william.pereira@me.com>
2018-11-07 17:39:59 -0200

    finishing auth w/ cookies

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const port = process.env.PORT || 5000;
 
-require('./services/passport.js')
-require('./routes/authRoutes.js')(app)
+require('./models/user');
+require('./services/passport.js');
+require('./routes/authRoutes.js')(app);
 
 app.listen(port);
